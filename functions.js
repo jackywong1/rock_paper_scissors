9ac966f295d69054d952dbf5ed87d0e1d831c4eb
Jacky Wong <z5115789@student.unsw.edu.au>
2023-02-03 01:40:56 +1100

    basic functions completed for scissors paper rock

--- functions.js
+++ functions.js
@@ -1,10 +1,11 @@
-const prompt=require("prompt-sync")({sigint:true});
+// const prompt=require("prompt-sync")({sigint:true});
 
 
 const numOfRounds = 5;
 const choiceArr = ["rock", "paper", "scissors"];
 let computerScore = 0;
 let playerScore = 0;
+
 
 
 // randomly select a value between 0 and 2
@@ -15,71 +16,94 @@ function getComputerChoice(computerChoiceArray) {
     return computerChoiceArray[computerChoice];
 }
 
-
-// ask for player input
-function playerSelection() {
-    let playerChoice = prompt("Choose either rock, paper or scissors: ");
-    if (playerChoice === "") {
-        console.log("TYPE SOMETHING IN YOU NOOB!");
-        return null;
-    }
-    return playerChoice.toLowerCase();
-}
-
-
 // play round and check if computer wins or player wins or if its a draw
 // returns message of whether the player won, lost or drew
-function playRound (computerPlays, playPlays) {
+function playRound (computerPlays, playerPlays) {
     let messageStr;
-    if (playPlays === null) {
+    if (playerPlays === null) {
         return null;
-    } else if ((computerPlays === playPlays) && (playPlays !== null)) {
+    } else if ((computerPlays === playerPlays) && (playerPlays !== null)) {
         messageStr = "This round is a tie!";
-    } else if ((computerPlays === "rock" && playPlays === "scissors")
-                || (computerPlays === "paper" && playPlays === "rock")
-                || (computerPlays === "scissors" && playPlays === "paper")) {
-        messageStr = `You lose! ${computerPlays} beats ${playPlays}`;
-    } else if ((playPlays === "rock" && computerPlays === "scissors")
-                || (playPlays === "paper" && computerPlays === "rock")
-                || (playPlays === "scissors" && computerPlays === "paper")) {
-        messageStr = `You win! ${playPlays} beats ${computerPlays}`;
-    }
-    console.log(messageStr);
-    return messageStr;
-}
-
-
-// play game for 5 round, update player and computer score
-function playGame() {
-    for (let i = 0; i < numOfRounds; i++) {
-        let computerChoosesThis = getComputerChoice(choiceArr);
-        let playerChoosesThis = playerSelection();
-        let message = playRound(computerChoosesThis, playerChoosesThis);
-        if (message === null) {
-            return null;
-        } else if (message.includes('win!')) {
-            playerScore++;
-        } else if (message.includes('lose!')) {
-            computerScore++;
-        } else {
-            //do nothing (tie)
-        }
+        displayGameMessage(messageStr);
+    } else if ((computerPlays === "rock" && playerPlays === "scissors")
+                || (computerPlays === "paper" && playerPlays === "rock")
+                || (computerPlays === "scissors" && playerPlays === "paper")) {
+        displayGameMessage(loseMessage ());
+        computerScore++;
+        // save and display the score as per scenario
+        displayComputerScore();
+    } else if ((playerPlays === "rock" && computerPlays === "scissors")
+                || (playerPlays === "paper" && computerPlays === "rock")
+                || (playerPlays === "scissors" && computerPlays === "paper")) {
+        displayGameMessage(winMessage ());
+        playerScore++;
+        // save and display the score as per scenario
+        displayPlayerScore();
     }
 }
 
-
-// call the game function to start the game
-playGame();
-
-
-// after the 5 rounds have been played,
-if (computerScore > playerScore) {
-    console.log("///// YOU LOSE! YOU TRASH KID! //////");
-} else if (computerScore < playerScore) {
-    console.log("///// YOU WIN! YOU JUST BEAT A COMPUTER LOL! /////");
-} else if (computerScore === playerScore) {
-    console.log("///// YOU TIED! HOW DO YOU EVEN TIE WITH A BOT BRO! /////");
+// display player's score on screen
+function displayPlayerScore () {
+    let playerPoints = document.querySelector('.player-score');
+    playerPoints.textContent = playerScore;
 }
+
+// display computer's score on screen
+function displayComputerScore () {
+    let computerPoints = document.querySelector('.computer-score');
+    computerPoints.textContent = computerScore;
+}
+
+// display game message
+function displayGameMessage (messageString) {
+    let gameMessage = document.querySelector('.game-message');
+    gameMessage.textContent = messageString;
+}
+
+// randomise the loser message
+function loseMessage () {
+    let loseMessageArr = [
+        'How do you lose to a bot bro?',
+        'Please...just bin yourself.',
+        'Do better.',
+        'Seriously?',
+        'My Grandma can do beat...'
+    ]
+    let loseMessageArrIndex = Math.floor(Math.random() * loseMessageArr.length);
+    return loseMessageArr[loseMessageArrIndex];
+}
+
+// randomise the winner message
+function winMessage () {
+    let winMessageArr = [
+        'Nice...you beat a bot.',
+        'Good job, you should pat yourself on the back.',
+        'Congratz, do you want an award with that?',
+        'Wow, you won.',
+        'Lucky choice rookie.'
+    ]
+    let winMessageArrIndex = Math.floor(Math.random() * winMessageArr.length);
+    return winMessageArr[winMessageArrIndex];
+}
+
+// declare which item you are querying
+// e.target always refers to the element that triggered the event
+// querySelector returns the first ELEMENT within the document that matches
+// the specified selector
+
+function displayRoundResult (e) {
+    let playerDecision = e.target.id;
+    return playRound(getComputerChoice(choiceArr), playerDecision);
+}
+
+const buttons = document.querySelectorAll('img');
+buttons.forEach((button) => button.addEventListener('click', e => {
+    // let roundEnd = displayRoundResult(e);
+    // trackScore(computerScore, playerScore, roundEnd);  
+    let playerSelection = e.target.id;  
+    playRound(getComputerChoice(choiceArr), playerSelection); 
+}));
+
 
 
 
